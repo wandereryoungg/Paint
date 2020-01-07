00c7c0b911c7bca0732e235fdacee64b40f4c2de
Charlie <173063877@qq.com>
2020-01-08 06:42:43 +0800

    简易画图软件

---
+++ app/src/main/java/com/young/paint/MainActivity.java
@@ -0,0 +1,59 @@
+package com.young.paint;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.security.Permission;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnClear;
+    Button btnSave;
+    MyView myView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
+
+        btnClear = findViewById(R.id.btn_clear);
+        btnSave = findViewById(R.id.btn_save);
+        myView = findViewById(R.id.my_view);
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myView.clearCanvas();
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myView.saveCanvas(v);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults[0] == PackageManager.PERMISSION_DENIED){
+                    Toast.makeText(this,"拒绝权限无法使用应用",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/young/paint/MyView.java
@@ -0,0 +1,145 @@
+package com.young.paint;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyView extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+
+    private Paint paint = new Paint();
+    private Path path = new Path();
+    private SurfaceHolder holder;
+    private Bitmap bitmap = Bitmap.createBitmap(400,800, Bitmap.Config.ARGB_8888);
+    private Canvas canvas;
+    private Canvas canvasTemp;
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init() {
+        holder = getHolder();
+        holder.addCallback(this);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.DKGRAY);
+        paint.setTextSize(40);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(5);
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        bitmap = Bitmap.createBitmap(getWidth(),getHeight(), Bitmap.Config.ARGB_8888);
+        canvasTemp = new Canvas(bitmap);
+        draw();
+
+    }
+
+    private void draw() {
+        try {
+            canvas = holder.lockCanvas();
+            if(holder!=null){
+                canvasTemp.drawColor(Color.WHITE);
+                canvasTemp.drawPath(path,paint);
+                canvas.drawBitmap(bitmap,0,0,null);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(holder !=null){
+                holder.unlockCanvasAndPost(canvas);
+            }
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(event.getX(),event.getY());
+                draw();
+                break;
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+                path.lineTo(event.getX(),event.getY());
+                draw();
+                break;
+
+        }
+        return true;
+    }
+
+    private String getTime(){
+        return new SimpleDateFormat("HHmmssSSS").format(new Date(System.currentTimeMillis()));
+    }
+
+    public void clearCanvas(){
+        path.reset();
+        draw();
+    }
+
+    public void saveCanvas(View view){
+        final FileOutputStream fos;
+        String fileName = getTime();
+        String filePath = Environment.getExternalStorageDirectory().getPath()+"/DCIM/Camera/"+fileName+".JPEG";
+        try {
+            fos = new FileOutputStream(new File(filePath));
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,100,fos);
+                }
+            }).start();
+
+            MediaScannerConnection.scanFile(getContext(),new String[]{filePath}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                @Override
+                public void onScanCompleted(String path, Uri uri) {
+                    Log.e("young","保存图片成功");
+                }
+            });
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
